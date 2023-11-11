66f41e5466a29d48f9acbfd9741ad6ae961149e8
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2023-11-11 22:07:12 +0700

    progress hourly adapter

--- app/src/main/java/com/example/weatherapp/Adapters/HourlyAdapters.kt
+++ app/src/main/java/com/example/weatherapp/Adapters/HourlyAdapters.kt
@@ -1,10 +1,12 @@
 package com.example.weatherapp.Adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherapp.R
 
 class HourlyAdapters : RecyclerView.Adapter<HourlyAdapters.viewHolder>() {
 
@@ -13,7 +15,8 @@ class HourlyAdapters : RecyclerView.Adapter<HourlyAdapters.viewHolder>() {
         var hourText: TextView? = null
         var tempText: TextView? = null
         var pic : ImageView? = null
-
+        var items : ArrayList<Hourly> = ArrayList()
+        var context : Context? = null;
         constructor(itemView: View, hourText: TextView?, tempText: TextView?, pic: ImageView?) : this(itemView) {
             super.itemView
 //            this.hourText = itemView.findViewById(R.id.)
@@ -23,7 +26,12 @@ class HourlyAdapters : RecyclerView.Adapter<HourlyAdapters.viewHolder>() {
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder{}
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder
+    {
+        var inflat : View = LayoutInflater.from(parent.context).inflate(R.layout.viewholder_hourly, parent, false)
+        context = parent.context
+        return viewHolder(inflat)
+    }
 
     override fun getItemCount(): Int {
         TODO("Not yet implemented")
