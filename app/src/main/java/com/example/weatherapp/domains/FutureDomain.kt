22c8de86e7746421e2974607b65b33747a4eaa6f
mairuu <kmr.mairuu@gmail.com>
2023-11-12 22:03:55 +0700

    reformat models

--- app/src/main/java/com/example/weatherapp/domains/FutureDomain.kt
+++ app/src/main/java/com/example/weatherapp/domains/FutureDomain.kt
@@ -1,6 +1,8 @@
 package com.example.weatherapp.domains
-
-
-
-data class FutureDomain(var day: String = "", var picPath: String = "", var status : String = "", var highTemp : Int = 0, var lowTemp : Int = 0) {
-}
+data class FutureDomain(
+    var day: String = "",
+    var picPath: String = "",
+    var status: String = "",
+    var highTemp: Int = 0,
+    var lowTemp: Int = 0
+)
--- app/src/main/java/com/example/weatherapp/domains/Hourly.kt
+++ app/src/main/java/com/example/weatherapp/domains/Hourly.kt
@@ -1,3 +1,7 @@
 package com.example.weatherapp.domains
 
-data class Hourly(var hour: String = "", var temp: Int = 0, var picPath: String = "")
+data class Hourly(
+    var hour: String = "",
+    var temp: Int = 0,
+    var picPath: String = ""
+)
