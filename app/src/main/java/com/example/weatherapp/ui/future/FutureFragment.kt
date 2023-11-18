b211bfda0bbea3d280dd7c9954f985f2daa26008
mairuu <kmr.mairuu@gmail.com>
2023-11-18 12:38:33 +0700

    optimize imports

--- app/src/main/java/com/example/weatherapp/ui/future/FutureFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/future/FutureFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -79,7 +78,7 @@ class FutureFragment : Fragment() {
             )
         }
 
-        view.findViewById<ConstraintLayout>(R.id.backButton)?.apply {
+        view.findViewById<View>(R.id.backButton)?.apply {
             setOnClickListener {
                 findNavController().popBackStack()
             }
